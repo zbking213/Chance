f7dda6aa98dc1256caa8f32b14fa670a0f981dc9
chance <785199825@qq.com>
2020-01-06 22:45:02 +0800

    0106:22:45

--- src/main/java/com/chance/controller/AopController.java
+++ src/main/java/com/chance/controller/AopController.java
@@ -1,4 +1,11 @@
 package com.chance.controller;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.concurrent.TimeUnit;
+
 public class AopController {
+    public static void main(String[] args) {
+        System.out.println("Time"+ TimeUnit.SECONDS);
+    }
 }
