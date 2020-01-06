22b586c6b4a9c5e3c067881693ebcca757d6e02e
chance <785199825@qq.com>
2020-01-06 22:57:15 +0800

    add 表达式within add README.md

--- src/main/java/com/chance/config/ZbkingAspectJ.java
+++ src/main/java/com/chance/config/ZbkingAspectJ.java
@@ -9,25 +9,28 @@ import org.springframework.stereotype.Component;
 //将这个类交给spring容器
 
 @Component
-//@Aspect  //声明一个切面
+@Aspect  //声明一个切面
 public class ZbkingAspectJ {
     //声明一个pointcut
     @Pointcut("execution(* com.chance.dao.*.*(..))")
-    public void pointCut(){
+    public void pointCutWithExecution(){
 
     }
+
+    @Pointcut("within(com.chance.dao.*)")
+    public void pointCutWithin(){
+
+    }
+
+
 
     /**
      *通知
      */
-    @Before("pointCut()")
+    @Before("pointCutWithin()")
     public void before(){
         System.out.println("search之前");
 
-    }
-    @After("pointCut()")
-    public void after(){
-        System.out.println("search之后");
     }
 
 }
